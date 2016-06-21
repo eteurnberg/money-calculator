968bf7ec293f8376db1150ff797a5961b8e969ba
Emil Teurnberg <e.teurnberg@gmail.com>
2016-06-21 13:42:40 +0200

    Changed route for presenting a list of ledgers

--- client/routes.jsx
+++ client/routes.jsx
@@ -4,7 +4,7 @@ import { mount } from 'react-mounter';
 import { MainLayout } from '../imports/layouts/MainLayout.jsx';
 import LedgerListing from '../imports/ui/LedgerListing.jsx';
 
-FlowRouter.route('/ledgers', {
+FlowRouter.route('/your-ledgers', {
     action() {
         mount(MainLayout, {
             content: (<LedgerListing />),
